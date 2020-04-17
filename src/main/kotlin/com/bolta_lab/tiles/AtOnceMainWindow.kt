a5cdf411ba8f3712c6a0cb16b08a930b60dcb490
bolta <freshness.and.speed.2007@gmail.com>
2020-04-18 01:51:02 +0900

    デバッグ用コードをコメントアウトで追加

--- src/main/kotlin/com/bolta_lab/tiles/AtOnceMainWindow.kt
+++ src/main/kotlin/com/bolta_lab/tiles/AtOnceMainWindow.kt
@@ -11,11 +11,21 @@ import kotlin.coroutines.experimental.buildSequence
 
 class AtOnceMainWindow(params: RenderingParameterSet, outFileName: String) : MainWindow(params, outFileName) {
 	override fun setup() {
+// デバッグ用にキャンバスの外側を見たい場合は有効にする
+//		this.translate(params.size.x.toFloat() / 2, params.size.y.toFloat() / 2)
+//		this.scale(0.5f)
+
 		val start = System.currentTimeMillis()
 //		this.g.noStroke()
 		render(this.g, this.params)
 		println((System.currentTimeMillis() - start).toString() + " ms")
 		this.save(this.outFileName)
+
+// デバッグ用にキャンバスの外側を見たい場合は有効にする（キャンバスの領域を示す）
+//		this.stroke(255f, 0f, 0f)
+//		this.noFill()
+//		this.strokeWeight(3f)
+//		this.rect(0f, 0f, params.size.x.toFloat(), params.size.y.toFloat())
 	}
 }
 
